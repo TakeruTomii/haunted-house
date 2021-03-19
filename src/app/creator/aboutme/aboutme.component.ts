29bac96b6f25ab57aaab39b84d805f67dc257c72
TakeruTomii <dev.haunted.house@gmail.com>
2021-03-19 17:10:06 +0900

    create Framework of Status and Legend of SkillSet

--- src/app/creator/aboutme/aboutme.component.ts
+++ src/app/creator/aboutme/aboutme.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
+  url_ipa_exam = "https://www.jitec.ipa.go.jp/2_01english/02examcategories.html";
+  url_python_exam = "https://www.pythonic-exam.com/basic-examination";
+
   constructor() { }
 
   ngOnInit(): void {
