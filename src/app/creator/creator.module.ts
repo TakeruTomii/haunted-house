f41d2bd9b8f8eef0c993810aaaa17c6bcf29b5d8
TakeruTomii <dev.haunted.house@gmail.com>
2021-03-19 19:10:37 +0900

    create carousel of skill set

--- src/app/creator/creator.module.ts
+++ src/app/creator/creator.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutMeComponent } from './aboutme/aboutme.component';
 
-
+// bootstrap
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [AboutMeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CarouselModule.forRoot()
   ]
 })
 export class CreatorModule { }
