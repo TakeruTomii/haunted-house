887cc2f9ea0f243ea3d0b65fc4c657102ef97155
TakeruTomii <dev.haunted.house@gmail.com>
2021-03-20 16:10:16 +0900

    install status display with randomized content

--- src/app/creator/aboutme/aboutme.component.ts
+++ src/app/creator/aboutme/aboutme.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { SerifComponent } from 'src/app/shared/serif/serif.component';
+import { SerifComponent } from '../../shared/serif/serif.component';
+import { STATUS_PATTERNS, PREFIX_PORTRAIT, IMG_PATH_ABOUT_ME, EXT_PORTRAIT, EXT_ATTR} from '../../shared/const';
 
 @Component({
   selector: 'app-aboutme',
@@ -17,10 +18,36 @@ export class AboutMeComponent implements OnInit {
   // prevent sliding of carousel
   stop_sliding = 0;
 
+  // Status variables
+  portrait_path : string;
+  attr_path : string;
+  str_val : string;
+  str_unit : string;
+  comment_content : string;
+  speacies : string;
+  exp_content : string;
+
   constructor(private modal: BsModalService) { }
 
   ngOnInit(): void {
-  }  // Open modal for serifs
+    // Initiate display items of status
+    // get current status randomly
+    let len_patterns = STATUS_PATTERNS.length;
+    let index = Math.floor(Math.random() * len_patterns);
+    let current_status = STATUS_PATTERNS[index];
+
+    // set display items
+    this.portrait_path = IMG_PATH_ABOUT_ME + PREFIX_PORTRAIT + current_status.attr + EXT_PORTRAIT;
+    this.attr_path = IMG_PATH_ABOUT_ME + current_status.attr + EXT_ATTR;
+    this.str_val = current_status.str_val;
+    this.str_unit = current_status.str_unit;
+    this.comment_content = current_status.comment;
+    this.speacies = current_status.speacies;
+    this.exp_content = current_status.exp;
+
+  }
+
+  // Open modal for serifs
   openSerifs(room : string, clicked : string){
 
     // Configs to open
--- src/app/shared/const.ts
+++ src/app/shared/const.ts
@@ -26,3 +26,53 @@ export const TRANSITABLE_ROOMS = {
   'casino': CasinoComponent,
   'garden': GardenComponent
 };
+
+// About Me
+// Status Patterns
+export const STATUS_PATTERNS = [
+  {
+    'attr': 'fire',
+    'str_val' : '200',
+    'str_unit' : '℃',
+    'comment': 'Does this placement truly lead users click it?  ――――',
+    'speacies': 'Zombie',
+    'exp': 'fire!! That\'s why he is called Mr. Fahrenheit.'
+  },
+  {
+    'attr': 'water',
+    'str_val' : '40,000',
+    'str_unit' : 'mL',
+    'comment': 'Like a stream, up to down. Scroll softly. Here it comes!!  ――――',
+    'speacies': 'Fairy',
+    'exp': 'water!!'
+  },
+  {
+    'attr': 'thunder',
+    'str_val' : '5,000',
+    'str_unit' : 'kW',
+    'comment': 'Which Function? Ah, I\'ve already pushed it.  ――――',
+    'speacies': 'Spell Caster',
+    'exp': 'thunder!!'
+  },
+  {
+    'attr': 'wind',
+    'str_val' : '700',
+    'str_unit' : 'km/h',
+    'comment': 'I feel the wind of change blows...  ――――',
+    'speacies': 'Winged Beast',
+    'exp': 'This card save 20% man-hours of labor in total.\n He works pretty speedy as a racing car like Lady Godiva and your project travels at the speed of light.'
+  },
+  {
+    'attr': 'ground',
+    'str_val' : '530,000',
+    'str_unit' : 'Centuries',
+    'comment': 'Let\'s adjust like this. Then everyone gets happier!   ――――',
+    'speacies': 'Dinosaur',
+    'exp': 'ground!!'
+  }
+]
+
+export const PREFIX_PORTRAIT = 'portrait_';
+export const IMG_PATH_ABOUT_ME = '../../../assets/img/';
+export const EXT_PORTRAIT = '.jpg';
+export const EXT_ATTR = '.png';
