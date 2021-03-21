8a9297a64f8ee3624df7a71c9ddb27fdb36fe7ba
TakeruTomii <dev.haunted.house@gmail.com>
2021-03-21 11:01:16 +0900

    ajust view of init-conf modal

--- src/app/init-conf/init-conf.component.ts
+++ src/app/init-conf/init-conf.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { InitConf } from '../shared/dto';
 import { INIT_LANGS, INIT_SOUNDS } from '../shared/const';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-init-conf',
   templateUrl: './init-conf.component.html',
   styleUrls: ['./init-conf.component.css']
 })
-export class InitConfComponent implements OnInit {
+export class InitConfComponent implements OnInit, AfterViewInit{
   // Language setting
   lang_selected='en';
   langs = INIT_LANGS;
@@ -16,11 +17,21 @@ export class InitConfComponent implements OnInit {
   // Sound setting
   sound_selected='on';
   sounds = INIT_SOUNDS;
+  modalRef: BsModalRef;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private modalService: BsModalService) { }
+  @ViewChild('init_modal') public init_modal: TemplateRef<any>;
 
-  ngOnInit(): void {
+  ngOnInit(): void {  }
+
+  ngAfterViewInit(){
+    this.openModal(this.init_modal);
   }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, {'backdrop': 'static'});
+  }
+
 
   // Pass initial settings to Loading Screen
   configure(){
