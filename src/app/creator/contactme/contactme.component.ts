6e08d1f80cc39b079f64b069aee77a6f4bea16aa
TakeruTomii <dev.haunted.house@gmail.com>
2021-04-18 17:37:58 +0900

    Add confirm and sent process

--- src/app/creator/contactme/contactme.component.ts
+++ src/app/creator/contactme/contactme.component.ts
@@ -11,6 +11,10 @@ export class ContactMeComponent implements OnInit {
     mail: '',
     message: ''
   }
+  bubble_content = "Fullfill the form and send Tommy your message!";
+  send_button_content = "Send";
+  isConfirmed = false;
+  isSend = false;
 
   constructor() { }
 
@@ -18,5 +22,17 @@ export class ContactMeComponent implements OnInit {
   }
 
   sendInquiry() {
+    if(this.isConfirmed){
+      this.bubble_content = "I send him ya messages! Ask me for next step!!"
+      this.send_button_content = "Not twice ;)"
+      this.isConfirmed = false;
+      this.isSend = true;
+      //TODO: Request Mail Sending Api
+    }
+    else {
+      this.bubble_content = "Confirm the content before sending. Can I truly send these contents?"
+      this.send_button_content = "No regrets, Send him now."
+      this.isConfirmed = true;
+    }
   }
 }
