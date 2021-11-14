1ac91ce06017210dbc8c837d7b9cd523f0d559e4
TakeruTomii <dev.haunted.house@gmail.com>
2021-11-14 16:58:36 +0900

    make character select animation

--- src/app/site/character-list/character-list.component.ts
+++ src/app/site/character-list/character-list.component.ts
@@ -19,6 +19,7 @@ export class CharacterListComponent implements OnInit {
   main_image_path : string;
   character_exp : string;
   select_characters : any[];
+  isCurtainOpen : boolean;
 
   // Characters Data
   character_data = CHARACTER_DATA;
@@ -26,8 +27,8 @@ export class CharacterListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.isCurtainOpen = false;
     this.setCurrentCharacter(0);
-
   }
 
   setCurrentCharacter(chara_id : number) {
@@ -35,25 +36,38 @@ export class CharacterListComponent implements OnInit {
     let id_selecter = "#character_" + chara_id + " img";
 
     //set movement when cliking characters
-    this.setCharacterOnTheStage(id_selecter);
+    this.animateCharacterChange(id_selecter);
 
-    this.character_id = chara_id;
-    this.character_name = chara['name'];
-    this.main_image_path = IMG_PATH_CHARACTER_LIST + chara['main_image_file'] + EXT_CHARACTER;
-    this.character_exp = chara['explanation'];
+    setTimeout(() => {
+      this.character_id = chara_id;
+      this.character_name = chara['name'];
+      this.main_image_path = IMG_PATH_CHARACTER_LIST + chara['main_image_file'] + EXT_CHARACTER;
+      this.character_exp = chara['explanation'];
+    }, 500)
+
   }
 
-  setCharacterOnTheStage(target: string){
+
+  animateCharacterChange(target: string){
+
+    // curtain movement
+    if(this.isCurtainOpen) {
+      this.characterChange('0');
+    } else {
+      this.openCurtain('0');
+    }
+
     // appear all characters
     var appear = anime.timeline({
       targets: ".select-background img",
       easing: 'easeInQuad',
       duration: 10
     });
+
     appear
     .add({
       opacity: 1,
-    })
+    });
 
     // selected character jump to the stage
     var on_the_stage = anime.timeline({
@@ -75,8 +89,46 @@ export class CharacterListComponent implements OnInit {
     })
     .add({
       translateY: 0
+    });
+
+  }
+
+  characterChange(delaytime: string) {
+    var close_open = anime.timeline({
+      targets: ".curtain",
+      easing: 'linear',
+      duration: 1200,
+      delay: delaytime
+    });
+
+    close_open
+    .add({
+      width: "100%",
+      duration: 500
+    })
+    .add ({
+      duration: 200
+    })
+    .add({
+      width: "0%",
+      duration: 500
+    })
+  }
+
+  openCurtain(delaytime: string) {
+    var open = anime.timeline({
+      targets: ".curtain",
+      easing: 'linear',
+      duration: 1500,
+      delay: delaytime
+    });
+
+    open
+    .add({
+      width: "0%"
     })
 
+    this.isCurtainOpen = true;
   }
 
 }
