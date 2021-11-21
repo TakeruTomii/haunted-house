d6c56f716ca93167170c5848fcb4ec5ff6d73f50
TakeruTomii <dev.haunted.house@gmail.com>
2021-11-21 19:04:03 +0900

    add  scroll gimmick in concept page

--- src/app/site/concept/concept.component.ts
+++ src/app/site/concept/concept.component.ts
@@ -1,4 +1,6 @@
+import { animation } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-concept',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./concept.component.css']
 })
 export class ConceptComponent implements OnInit {
+  isOpenLastAccrodion = false;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    // gimmick in scroll
+    // show mouths background when scroll to the end with last accordion open
+    window.addEventListener('scroll', () => {
+      if(this.isOpenLastAccrodion) {
+        let scrollHeight = Math.max(
+          document.body.scrollHeight, document.documentElement.scrollHeight,
+          document.body.offsetHeight, document.documentElement.offsetHeight,
+          document.body.clientHeight, document.documentElement.clientHeight
+        );
+        let pageMostBottom = scrollHeight - window.innerHeight - 5;
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        // action when scroll to the bottom
+        if (scrollTop >= pageMostBottom) {
+          document.getElementById('main-container').classList.add("last-scroll");
+        } else {
+          document.getElementById('main-container').classList.remove("last-scroll");
+        }
+      }
+    });
+  }
+
+  setOpenStateOfLastAccordion() {
+    this.isOpenLastAccrodion = !(this.isOpenLastAccrodion);
   }
 
 }
