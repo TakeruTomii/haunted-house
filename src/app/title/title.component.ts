4d16c6caa62519857ded012481b573b92be79b80
TakeruTomii <dev.haunted.house@gmail.com>
2021-11-23 17:19:22 +0900

    add logo movement on title screen

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-title',
@@ -10,6 +11,25 @@ export class TitleComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log("ngOnInit");
+
+      //Logo movement
+      var logo = anime.timeline({
+        targets:"#title-logo img",
+        easing: 'linear'
+      });
+
+      logo
+      .add({
+        translateY: -50
+      })
+      .add({
+        opacity: 1,
+        translateY: 0,
+        duration: 1000
+      })
+      console.log("done logo movement");
+      //Button movement
   }
 
 }
