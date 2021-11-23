9f2a765ed5a515da671eec527345e8aa5bd1b542
TakeruTomii <dev.haunted.house@gmail.com>
2021-11-23 19:19:32 +0900

    just clean up code

--- src/app/creator/creator.module.ts
+++ src/app/creator/creator.module.ts
@@ -4,10 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AboutMeComponent } from './aboutme/aboutme.component'
 import { AccordionModule } from 'ngx-bootstrap/accordion';
-
-// bootstrap
-import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { ContactMeComponent } from './contactme/contactme.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [AboutMeComponent, ContactMeComponent],
