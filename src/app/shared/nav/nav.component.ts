d3ecb39f47c2342dea846ee6fe1489a0100ac863
TakeruTomii <dev.haunted.house@gmail.com>
2021-11-26 00:27:55 +0900

    improved nav bar

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -19,6 +19,20 @@ export class NavComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var volume = document.getElementById('sound_input');
+    var target = document.getElementById('vol_value');
+    var rangeValue =  (volume, target) => {
+      return (event) => {
+        target.innerHTML = volume.value;
+        var icon = document.getElementById('sound-icon');
+        if(volume.value === "0"){
+          icon.classList.add('volume-zero');
+        }else{
+          icon.classList.remove('volume-zero');
+        }
+      }
+    }
+    volume.addEventListener('input', rangeValue(volume, target));
   }
 
 }
