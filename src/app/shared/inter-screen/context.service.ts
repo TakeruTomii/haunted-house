5929127f4a8208287b31afe9cc354e28326eb46a
TakeruTomii <dev.haunted.house@gmail.com>
2021-12-19 22:06:25 +0900

    enable to take over sound setting when reload

--- src/app/shared/inter-screen/context.service.ts
+++ src/app/shared/inter-screen/context.service.ts
@@ -10,11 +10,23 @@ export class ContextService {
   constructor() { }
 
   setSound(sound:SoundInfo) {
-    this._sound = new SoundInfo();
     this._sound = sound;
+    sessionStorage.setItem('is_sound_on', String(sound.is_sound_on));
+    sessionStorage.setItem('volume', String(sound.volume));
+    sessionStorage.setItem('bgm_filename', sound.bgm_filename);
   }
 
   getSound() {
+    if(!this._sound) {
+      const str_is_sound_on = sessionStorage.getItem('is_sound_on');
+      const str_volume = sessionStorage.getItem('volume');
+      const bgm_filename = sessionStorage.getItem('bgm_filename');
+      this._sound = {
+        is_sound_on: (str_is_sound_on === 'true') ? true : false,
+        volume: Number(str_volume).valueOf(),
+        bgm_filename: bgm_filename
+      }
+    }
     return this._sound;
   }
 }
