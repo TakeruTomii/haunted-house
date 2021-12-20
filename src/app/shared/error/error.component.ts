2e4f07cdf05789cd843b9d1bfc44a6f4a49d65c7
TakeruTomii <dev.haunted.house@gmail.com>
2021-12-20 22:25:26 +0900

    add serifs to error screen

--- src/app/shared/error/error.component.ts
+++ src/app/shared/error/error.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { SerifComponent } from '../serif/serif.component';
 
 @Component({
   selector: 'app-error',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./error.component.css']
 })
 export class ErrorComponent implements OnInit {
+  modalRef: BsModalRef;
 
-  constructor() { }
+  constructor(private modal: BsModalService) { }
 
   ngOnInit(): void {
+    this.openSerifs("Error", "Akabeko");
+  }
+
+  // Open modal for serifs
+  openSerifs(room : string, clicked : string){
+
+    // Configs to open
+    let initialState = {
+      room: room,
+      clicked: clicked
+    };
+
+    let show_config = {
+      initialState,
+      class: 'serif-modal',
+      animated: false
+    }
+
+    this.modalRef = this.modal.show(SerifComponent, show_config);
   }
 
 }
